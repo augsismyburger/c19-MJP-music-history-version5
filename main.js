6705ae9a3df054688dfd01b104ac95451df6d7c9
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-04-19 08:05:54 -0500

    Adding requirements for version 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var target = document.getElementById("song-list");
+var songListDiv = document.getElementById("song-list");
 var music = {};
 var songs = [];
 function MakeMusic(name, artist, album) {
@@ -21,5 +21,34 @@ songs.forEach(function(i) {
        i = i.replace(x, "");
     });
     var song = new MakeMusic(i.slice(0, i.indexOf(" -")), i.slice(i.indexOf("by ") + 3, i.indexOf(" on the ")), i.slice(i.indexOf("album") + 6, i.length));
-    target.innerHTML += "<li id='js-song'>" + song.name + "</li>" + "<li class='js-info'>" + song.artist + "</li>" + "<li class='js-info'>|</li>" + "<li class='js-info'>" + song.album + "</li>";
+    songListDiv.innerHTML += "<li id='js-song'>" + song.name + "</li>" + "<li class='js-info'>" + song.artist + "</li>" + "<li class='js-info'>|</li>" + "<li class='js-info'>" + song.album + "</li>";
 });
+// GRABBING NEW SONGS FROM DOM INPUT
+var newSong = document.getElementById("new-song");
+var newArtist = document.getElementById("new-artist");
+var newAlbum = document.getElementById("new-album");
+var addMusicButton = document.getElementById("new-song-button");
+addMusicButton.addEventListener("click", function() {
+    var song = new MakeMusic(newSong.value, newArtist.value, newAlbum.value);
+    songListDiv.innerHTML += "<li id='js-song'>" + song.name + "</li>" + "<li class='js-info'>" + song.artist + "</li>" + "<li class='js-info'>|</li>" + "<li class='js-info'>" + song.album + "</li>";
+    newSong.value = "";
+    newArtist.value = "";
+    newAlbum.value = "";
+});
+
+// SPA SWITCHERONI
+var addMusic = document.getElementById("add-music");
+var viewMusic = document.getElementById("view-music");
+var mainContent = document.getElementById("main-content");
+var hiddenContent = document.getElementById("add-music-div");
+addMusic.addEventListener("click", function(event) {
+    console.log(event);
+    mainContent.classList.add("hidden");
+    hiddenContent.classList.remove("hidden");
+});
+viewMusic.addEventListener("click", function(event) {
+    console.log(event);
+    mainContent.classList.remove("hidden");
+    hiddenContent.classList.add("hidden");
+});
+
